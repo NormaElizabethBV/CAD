366d00fb06b5a7a523048c008c614d3bea021b12
Norma Elizabeth BV <36415416+NormaElizabethBV@users.noreply.github.com>
2019-10-14 13:33:01 -0500

    Update omp_1.c

--- omp_1.c
+++ omp_1.c
@@ -1,14 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<omp.h>
 
  int main()
 
 {
-	#pragma omp parallel
+	//omp_set_num_threads(5);
+	
+	int i=5; //Compartidas
+	
+	#pragma omp parallel num_threads(5) //Clausula
 	{
+		int i=5; //Variable privada, cuando sale no la reconocen funciones externas
 
-		printf("Hola Mundo \n");
+		printf("Hola Mundo :) \n");
+		i=i+1;
+		printf("%d \n",i);
 
 	}
+		printf("%d \n",i);
 		return 0;
 } 
+
+
+//Compilar como -fopenmp archivo.c -o h
+// Ejecutar como ./h
+
+//export OMP_NUM_THREADS=4 
+
+//www.openmp.org
